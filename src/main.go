18ddef5e73015adf39172d341ec5eb7442ee935b
Radhesh Gupta <radeshg04@gmail.com>
2026-05-18 01:56:22 +0000

    faet: added parse functions

--- src/main.go
+++ src/main.go
@@ -8,12 +8,22 @@ import (
 func main() {
 	file, err := os.ReadFile("../tests/hello.rad")
 	if err != nil {
-		fmt.Println(err.Error()) // Handle error
+		fmt.Println(err.Error())
+		return
 	}
 	fmt.Printf("%s\n", string(file))
-	fmt.Printf("Lexer Output: \n")
+	fmt.Printf("Lexer Output:\n")
+	fmt.Printf("%-4s %-16s %s\n", "Line", "Type", "Lexeme")
+	fmt.Printf("%-4s %-16s %s\n", "----", "----", "------")
 	tokens := Lex(string(file))
 	for _, val := range tokens {
 		fmt.Println(val)
 	}
+	fmt.Printf("\nAST:\n")
+	ast, err := Parse(tokens)
+	if err != nil {
+		fmt.Printf("Parse error: %v\n", err)
+		return
+	}
+	printAST(ast)
 }
