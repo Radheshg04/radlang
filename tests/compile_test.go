d65064008f3f00068d4c7c354aa76bf88f3508c6
Radhesh Gupta <radeshg04@gmail.com>
2026-05-19 20:29:00 +0000

    test: update integration test post-refactor

--- tests/compile_test.go
+++ tests/compile_test.go
@@ -1,23 +1,29 @@
-package main
+package tests
 
 import (
 	"os"
 	"testing"
+
+	"radlang/interpreter"
+	"radlang/lexer"
+	"radlang/parser"
+	"radlang/semantic"
 )
 
 func TestCompileHello(t *testing.T) {
-	src, err := os.ReadFile("../tests/hello.rad")
+	src, err := os.ReadFile("arithmetic.rad")
 	if err != nil {
 		t.Fatalf("read hello.rad: %v", err)
 	}
 
-	tokens := Lex(string(src))
-	ast, err := Parse(tokens)
+	tokens := lexer.Lex(string(src))
+	ast, err := parser.Parse(tokens)
 	if err != nil {
 		t.Fatalf("parse error: %v", err)
 	}
-	err = Analyze(ast)
+	err = semantic.Analyze(ast)
 	if err != nil {
 		t.Fatalf("semantic analysis returned: %v", err)
 	}
+	interpreter.Interpret(ast)
 }
---
+++ tests/go.mod
@@ -0,0 +1,7 @@
+module radlang/tests
+
+go 1.22.2
+
+require radlang v0.0.0
+
+replace radlang => ../src
