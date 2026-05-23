5b4e8f75e1a766f9861f205606c02f35b6b2ff8b
Radhesh Gupta <radeshg04@gmail.com>
2026-05-23 07:20:28 +0000

    feat(parser): expand AST and grammar for mk2 language rules

--- core/parser/ast.go
+++ core/parser/ast.go
@@ -3,13 +3,43 @@ package parser
 import "radlang/token"
 
 type Program struct {
-	Functions []*Func_Decl
+	Functions  []*Func_Decl
+	Structs    []*Struct_Decl
+	Interfaces []*Interface_Decl
 }
 
 // Add returntype and params
 type Func_Decl struct {
-	Name string
-	Body *Block
+	Signature *Func_Signature
+	Body      *Block
+}
+
+type Func_Signature struct {
+	Name    *string
+	Params  []*Param
+	Returns []token.TokenType
+}
+
+type Param struct {
+	Name *string
+	Type token.TokenType
+}
+
+type Struct_Decl struct {
+	Name *string
+	Body *STRUCT_DEF_BLOCK
+}
+
+type Interface_Decl struct {
+	Name *string
+	Body *INTERFACE_DEF_BLOCK
+}
+
+type STRUCT_DEF_BLOCK struct {
+	Statement_Group []*Decl_stmt
+}
+type INTERFACE_DEF_BLOCK struct {
+	Statement_Group []*Func_Signature
 }
 
 type Block struct {
@@ -26,19 +56,26 @@ type Expression interface {
 	exprNode()
 }
 
+type Assign_stmt struct {
+	Targets []string
+	Values  []Expression
+	Op      token.TokenType
+}
+
+func (*Assign_stmt) stmtNode() {}
+
+type Expr_stmt struct {
+	Expression Expression
+}
+
+func (*Expr_stmt) stmtNode() {}
+
 type Decl_stmt struct {
 	Name string
 	Type token.TokenType
 }
 
 func (*Decl_stmt) stmtNode() {}
-
-type Assign_stmt struct {
-	Target string
-	Value  Expression
-}
-
-func (*Assign_stmt) stmtNode() {}
 
 // Handles x++ and x--
 type Update_stmt struct {
@@ -48,11 +85,33 @@ type Update_stmt struct {
 
 func (*Update_stmt) stmtNode() {}
 
-type Expr_stmt struct {
-	Expr Expression
+type Control_stmt struct {
+	Expression Expression
+	IfBlock    *Block
+	ElseStmt   *Control_stmt
+	ElseBlock  *Block
 }
 
-func (*Expr_stmt) stmtNode() {}
+func (*Control_stmt) stmtNode() {}
+
+type Jump_stmt struct {
+	Type token.TokenType
+}
+
+func (*Jump_stmt) stmtNode() {}
+
+type Return_stmt struct {
+	Returns []Expression
+}
+
+func (*Return_stmt) stmtNode() {}
+
+type Loop_stmt struct {
+	Expression Expression
+	Loop_block *Block
+}
+
+func (*Loop_stmt) stmtNode() {}
 
 type Identifier_expr struct {
 	Name string
@@ -71,13 +130,14 @@ func (*Binary_expr) exprNode() {}
 // Function Call expr
 type Call_expr struct {
 	Name string
+	Args []Expression
 }
 
 func (*Call_expr) exprNode() {}
 
-type Number_lit struct {
-	Value string
+type Lit_val struct {
+	Value interface{} // store parsed values
 	Type  token.TokenType
 }
 
-func (*Number_lit) exprNode() {}
+func (*Lit_val) exprNode() {}
